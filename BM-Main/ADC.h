006b62478b7002e86c46df869db6d492313d2a4a
Sebastian D <19Sebastian95@gmx.de>
2018-06-01 22:00:49 +0200

    adc

--- BM-Main/ADC.h
+++ BM-Main/ADC.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "adc_hall.h"
+#include "Utils/Utils.h"
+#include <avr/interrupt.h>
 
 namespace BMCPP
 {
@@ -10,62 +12,15 @@ namespace BMCPP
 			SINGLE_RUN
 		};
 
-		template<int N> struct adcMUX;
-
-		template<> 
-		struct adcMUX<0>
-		{
-			static constexpr uintptr_t value = 0x0;
-		};
-		template<>
-		struct adcMUX<1>
-		{
-			static constexpr uintptr_t value = 0x1;
-		};
-		template<>
-		struct adcMUX<2>
-		{
-			static constexpr uintptr_t value = 0x2;
-		};
-		template<>
-		struct adcMUX<3>
-		{
-			static constexpr uintptr_t value = 0x3;
-		};
-		template<>
-		struct adcMUX<4>
-		{
-			static constexpr uintptr_t value = 0x4;
-		};
-		template<>
-		struct adcMUX<5>
-		{
-			static constexpr uintptr_t value = 0x5;
-		};
-		template<>
-		struct adcMUX<6>
-		{
-			static constexpr uintptr_t value = 0x6;
-		};
-		template<>
-		struct adcMUX<7>
-		{
-			static constexpr uintptr_t value = 0x7;
-		};
-
-		template<ADCMeasurementType measurement_type, uint8_t prescaler_value, typename UC = __DEFAULT_MMCU__ >
+		template<uint8_t prescaler_value = 8, uint8_t adc_number = 0, ADCMeasurementType measurement_type = CONTINUOUS, typename UC = __DEFAULT_MMCU__ >
 		class ADConverter final
 		{
 			static_assert(prescaler_value >= 2 && prescaler_value <= 128, "Prescaler value has to be between 2 and 128!");
 			static_assert((prescaler_value & (prescaler_value - 1)) == 0, "Prescaler value has to be a power of 2!");
+			static_assert(adc_number <= UC::ADConverter::count, "The ADC number is exceeding the amount of ADCs available!");
 
-			static constexpr uint8_t adcsra = (
-				(static_cast<uint8_t>(UC::ADConverter::adcsra::aden)) |
-				(static_cast<uint8_t>(UC::ADConverter::adcsra::adie))
-				);
-
-			constexpr uint8_t get_prescaler_value(uint8_t i) {
-				switch (i) {
+			constexpr uint8_t get_prescaler_value() {
+				switch (prescaler_value) {
 					case 2:		return 0;
 					case 4:		return (static_cast<uint8_t>(UC::ADConverter::adcsra::adps1));
 					case 8:		return (static_cast<uint8_t>(UC::ADConverter::adcsra::adps1) | static_cast<uint8_t>(UC::ADConverter::adcsra::adps0));
@@ -75,19 +30,40 @@ namespace BMCPP
 					case 128:	return (static_cast<uint8_t>(UC::ADConverter::adcsra::adps2) | static_cast<uint8_t>(UC::ADConverter::adcsra::adps1) | static_cast<uint8_t>(UC::ADConverter::adcsra::adps0));
 				}
 			}
+
+			static constexpr uint8_t maximum_active_adcs = 0;
+			static constexpr uint8_t adcsra = (
+				(static_cast<uint8_t>(UC::ADConverter::adcsra::aden)) |
+				(static_cast<uint8_t>(UC::ADConverter::adcsra::adie)) |
+				(static_cast<uint8_t>(UC::ADConverter::adcsra::adsc)) |
+				get_prescaler_value()
+			);
+
+			static volatile uint16_t adc_result[maximum_active_adcs];
+
 		public:
-			template<typename... ADCChannels>
-			static void start() {
+			template<uint8_t... ADCChannels>
+			static void startChannels() {
 				static_assert(sizeof...(ADCChannels) <= 8, "There are a Maximum of 8 ADCs!");
-				volatile typename UC::Mem_Width* adcsra_adr = (typename UC::Mem_Width*)BMCPP::Hal::Hardware_Adc<0>::adcsra();
+				if constexpr(maximum_active_adcs < sizeof...(ADCChannels)) {
+					maximum_active_adcs = sizeof...(ADCChannels);
+				}
+
+				volatile typename UC::Mem_Width* adcsra_adr = (typename UC::Mem_Width*)BMCPP::Hal::Hardware_Adc<adc_number>::adcsra();
 				*adcsra_adr = adcsra;
 			}
 			static void stop() {
 
 			}
 			//TODO
-			template<typename Channel>
-			uint16_t getValue();
+			template<uint8_t Channel>
+			static uint16_t getValue() {
+				return adc_result[Channel];
+			}
 		};
 	}
+}
+
+ISR(ADC_vect)
+{
 }
