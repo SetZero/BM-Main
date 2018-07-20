df818780ac4366d90fbbebf31cf7524e1f28a5af
Keven <keven.kloeckner@gmail.com>
2018-07-20 19:11:25 +0200

    added Descriptions

--- BM-Main/JoyStick.h
+++ BM-Main/JoyStick.h
@@ -10,18 +10,27 @@ class JoyStick {
 	JoyStick() = delete;
 
 public:
+	/*
+	* Description: tests if the Controller is moved to left
+	*/
 	static inline bool isLeft() {
 		return adc::template getValue<ChannelNumberX>() < 400;
 	}
-
+	/*
+	* Description: tests if the Controller is moved to right
+	*/
 	static inline bool isRight() {
 		return adc::template getValue<ChannelNumberX>() > 600;
 	}
-
+	/*
+	* Description: tests if the Controller is moved to up
+	*/
 	static inline bool isUp() {
 		return adc::template getValue<ChannelNumberY>() > 600;
 	}
-
+	/*
+	* Description: tests if the Controller is moved to down
+	*/
 	static inline bool isDown() {
 		return adc::template getValue<ChannelNumberY>() < 400;
 	}
